4e6c91deb2371fd91521d710e5b9a4a81430b998
Internetimagery <jason.dixon.email@gmail.com>
2015-09-01 02:27:12 +1200

    more broad getting vertices information

--- __init__.py
+++ __init__.py
@@ -7,6 +7,7 @@ from re import findall
 import maya.api.OpenMaya as om
 import maya.api.OpenMayaUI as omui
 
+
 class Selector(object):
     """
     Set it all up
@@ -16,18 +17,31 @@ class Selector(object):
         s.mesh = ""
         s.sjob = cmds.scriptJob(e=["SelectionChanged", s.selectionChanged], ro=True)
         s.tool = "TempSelectionTool"
-        s.selectionFree = True
 
     """
     Monitor selection changes
     """
     def selectionChanged(s):
         print "selection Changed"
-        if s.selectionFree:
-            selection = cmds.ls(sl=True)
-            if selection and selection[0] in s.meshes:
+        selection = cmds.ls(sl=True)
+        if cmds.currentCtx() != s.tool:
+            if selection and len(selection) == 1 and selection[0] in s.meshes:
                 s.switchTool()
                 s.mesh = selection[0]
+            else:
+                for mesh in s.meshes:
+                    s.setColour(mesh)
+        # print cmds.ls(selection, st=True)
+
+    """
+    Set vertex colour on selection
+    """
+    def setColour(s, mesh, colour=None):
+        if colour:
+            cmds.setAttr("%s.displayColors" % mesh, 1)
+            cmds.polyColorPerVertex(rgb=colour)
+        else:
+            cmds.setAttr("%s.displayColors" % mesh, 0)
 
     """
     Switch to our custom picker tool
@@ -39,17 +53,16 @@ class Selector(object):
         cmds.draggerContext(
             s.tool,
             name=s.tool,
-            pressCommand=s.makeSelection,
+            releaseCommand=s.makeSelection,
+            dragCommand=s.updateSelectionPreview,
             cursor="hand")
         cmds.setToolTo(s.tool)
-        s.selectionFree = False
 
     """
     Switch back to the last tool used
     """
     def revertTool(s):
         cmds.setToolTo(s.lastTool)
-        s.selectionFree = True
         cmds.refresh()
 
     """
@@ -57,26 +70,44 @@ class Selector(object):
     """
     def makeSelection(s):
         if s.mesh:
-            try:
-                # Grab screen co-ords
-                viewX, viewY, viewZ = cmds.draggerContext(s.tool, q=True, ap=True)
-                # Set up empty vectors
-                position = om.MPoint()
-                direction = om.MVector()
-                # Convert 2D positions into 3D positions
-                omui.M3dView().active3dView().viewToWorld(int(viewX), int(viewY), position, direction)
-                selection = om.MSelectionList()
-                selection.add(s.mesh)
-                dagPath = selection.getDagPath(0)
-                fnMesh = om.MFnMesh(dagPath)
-                # Shoot a ray and check for intersection
-                intersection = fnMesh.closestIntersection(om.MFloatPoint(position), om.MFloatVector(direction), om.MSpace.kWorld, 99999, False)
-                if intersection:
-                    # Pick nearest bone with influence
-                    s.pickSkeleton(intersection)
-            except IOError:
-                print "Could not make selection. Were you selecting the right object?"
+            intersection = s.getPointer(s.mesh, s.tool)
+            if intersection:
+                # Pick nearest bone with influence
+                bone, verts = s.pickSkeleton(intersection)
+                if bone:
+                    cmds.select(s.mesh, r=True)
+                    s.setColour(s.mesh, (0.5,0.5,0.5))
+                    cmds.select(bone, r=True)
         s.revertTool()
+
+    """
+    Update display
+    """
+    def updateSelectionPreview(s):
+        print "dragging"
+
+    """
+    Get Mouse in 3D
+    """
+    def getPointer(s, mesh, tool):
+        try:
+            # Grab screen co-ords
+            viewX, viewY, viewZ = cmds.draggerContext(tool, q=True, ap=True)
+            # Set up empty vectors
+            position = om.MPoint()
+            direction = om.MVector()
+            # Convert 2D positions into 3D positions
+            omui.M3dView().active3dView().viewToWorld(int(viewX), int(viewY), position, direction)
+            selection = om.MSelectionList()
+            selection.add(mesh)
+            dagPath = selection.getDagPath(0)
+            fnMesh = om.MFnMesh(dagPath)
+            # Shoot a ray and check for intersection
+            intersection = fnMesh.closestIntersection(om.MFloatPoint(position), om.MFloatVector(direction), om.MSpace.kWorld, 99999, False)
+            return intersection
+        except RuntimeError:
+            print "Could not find point."
+
 
     """
     Pick a bone from a point in space
@@ -84,28 +115,59 @@ class Selector(object):
     def pickSkeleton(s, intersection):
         hitPoint, hitRayParam, hitFace, hitTriangle, hitBary1, hitBary2 = intersection
         if hitTriangle != -1:
+            # Grab original selection
+            selection = cmds.ls(sl=True)
             # Grab skin Cluster
             skin = mel.eval("findRelatedSkinCluster %s" % s.mesh)
             if skin:
                 # Get Face selected
                 cmds.select("%s.f[%s]" % (s.mesh, hitFace), r=True)
-                face = cmds.polyInfo(fv=True)
-                # Get Vertexes
-                verts = ["%s.vtx[%s]" % (s.mesh, v) for v in findall(r"\s(\d+)\s", face[0])]
-                # Get Joints
-                cmds.select(verts, r=True)
-                joints = cmds.skinPercent(skin, q=True, t=None)
-                # Get weights
-                weights = sorted(
-                    [(j, cmds.skinPercent(skin, t=j, q=True)) for j in joints],
-                    key=lambda x: x[1],
-                    reverse=True)
-                cmds.select(weights[0][0], r=True)
+                # face = cmds.polyInfo(fv=True)
+
+                joints = cmds.skinCluster(skin, q=True, wi=True)
+                weights = {}
+                verts = [v for v in findall(r"\s(\d+)\s", cmds.polyInfo(fv=True)[0])]
+                vertWeights = {}
+                for j in cmds.listAttr("%s.weightList" % skin, multi=True):
+                    parse = findall(r"\[(\d+)\]", j)
+                    if 1 < len(parse):
+                        joint = joints[int(parse[1])]
+                        weights[joint] = weights.get(joint, [])
+                        weights[joint].append(parse[0])
+                        if parse[0] in verts:
+                            vertWeights[joint] = vertWeights.get(joint, 0)
+                            vertWeights[joint] += cmds.getAttr("%s.%s" % (skin, j))
+                highestInfluence = max(vertWeights,key = lambda x: vertWeights.get(x))
+                return highestInfluence, weights[highestInfluence]
+
+
+                #
+                # # Get Vertexes
+                # verts = ["%s.vtx[%s]" % (s.mesh, v) for v in findall(r"\s(\d+)\s", face[0])]
+                # # Get Joints
+                # cmds.select(verts, r=True)
+                # joints = cmds.skinPercent(skin, q=True, t=None)
+                # # Get weights
+                # weights = sorted(
+                #     [(j, cmds.skinPercent(skin, t=j, q=True)) for j in joints],
+                #     key=lambda x: x[1])
+                # # Get other weights of the bone
+                # cmds.select(selection)
+                # return weights[-1][0]
             else:
                 print "No skin found."
         else:
             print "Nothing to select."
+        return None, None
 
 sel = cmds.ls(type="transform")
 sel = ["pCylinder1"]
 go = Selector(sel)
+
+# result = {}
+# for i in cmds.listAttr("skinCluster1.weightList", multi=True):
+#     match = findall(r"\[(\d+)\]", i)
+#     if len(match) == 1:
+#         result[match[0]] = []
+#     else:
+#         result[match[0]].append(match[1])
